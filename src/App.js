4c6ccc589a9a4ffd4d93b0e100ee3efa72e354bb
Davyd Kaio <davyd.kaio@gmail.com>
2022-12-29 16:35:32 -0300

    feat: :sparkles: Adding Sidebar and Application Body

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import React from "react";
 import "./App.css";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import MailList from "./MailList";
+import Mail from "./Mail";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Sidebar />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+
+        <div class="app__body">
+          <Sidebar />
+
+          <Routes>
+            <Route path="/" element={<MailList />} />
+            <Route path="/mail" element={<Mail />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/MailList.js
@@ -0,0 +1,61 @@
+import { Checkbox } from "@mui/material";
+import {
+  ArrowDropDown,
+  ChevronLeft,
+  ChevronRight,
+  Inbox,
+  KeyboardHide,
+  LocalOffer,
+  MoreVert,
+  People,
+  Redo,
+  Settings,
+} from "@mui/icons-material";
+import { IconButton } from "@mui/material";
+import React from "react";
+import "./MailList.css";
+import Section from "./Section";
+
+function MailList() {
+  return (
+    <div className="mailList">
+      <div class="mailList__settings">
+        <div class="mailList__settingsLeft">
+          <Checkbox />
+          <IconButton>
+            <ArrowDropDown />
+          </IconButton>
+          <IconButton>
+            <Redo />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+
+        <div class="mailList__settingsRight">
+          <IconButton>
+            <ChevronLeft />
+          </IconButton>
+          <IconButton>
+            <ChevronRight />
+          </IconButton>
+          <IconButton>
+            <KeyboardHide />
+          </IconButton>
+          <IconButton>
+            <Settings />
+          </IconButton>
+        </div>
+      </div>
+
+      <div class="mailList__sections">
+        <Section Icon={Inbox} title="Primary" color="red" selected />
+        <Section Icon={People} title="Social" color="#1A73E8" />
+        <Section Icon={LocalOffer} title="Promotions" color="green" />
+      </div>
+    </div>
+  );
+}
+
+export default MailList;
